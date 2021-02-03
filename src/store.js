4e91bf1d1a97f09833dc8571dee86e04c5dd0487
daricocity <daricocity@gmail.com>
2021-02-04 00:50:45 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -11,35 +11,3 @@ if (process.env.NODE_ENV === 'development') {
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export default store;
-
-// import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
-// import rootReducer from './reducers';
-// import { createStore, applyMiddleware, compose } from 'redux';
-
-// const initialState = {};
-// const middlewares = [thunk];
-// let store;
-
-// if (process.env.NODE_ENV === 'development'){
-//     middlewares.push(logger);
-// }
-
-// if (window.navigator.userAgent.includes("Chrome")){
-//     store = createStore(
-//         rootReducer, 
-//         initialState, 
-//         compose(
-//             applyMiddleware(...middlewares),
-//             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//         )
-//     );
-// } else {
-//     store = createStore(
-//         rootReducer, 
-//         initialState, 
-//         compose(applyMiddleware(...middlewares))
-//     );
-// }
-
-// export default store;
