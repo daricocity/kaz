7fa8ae26b23837c602cc755b098198f935dcefb5
daricocity <daricocity@gmail.com>
2021-02-04 01:08:59 +0100

    Update setupUtils.js

--- src/utils/setupUtils.js
+++ src/utils/setupUtils.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Production Server
-// export const baseUrl = 'https://kazubarmaketapi.pythonanywhere.com';
+export const baseUrl = 'https://kazubarmaketapi.pythonanywhere.com';
 
 // Development Server
-export const baseUrl = 'http://127.0.0.1:8000';
+// export const baseUrl = 'http://127.0.0.1:8000';
 
 export const setJWTToken = token => {
     // if token exits, then set it to the header
